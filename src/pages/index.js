f139690668249cb5c22ba865848bab422b36f6af
Ajay Kumar <56859399+ajaysourcedigital@users.noreply.github.com>
2026-06-21 13:39:50 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World.{" "}
+      Hello Ajay.{" "}
       <Link href="/about">
         About
       </Link>

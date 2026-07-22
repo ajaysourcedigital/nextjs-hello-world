c35ec80d96956f69342705d48d3ab32becc5da06
Ajay Kumar <56859399+ajaysourcedigital@users.noreply.github.com>
2026-07-22 11:18:25 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World.{" "}
+      Hello World, i am here.{" "}
       <Link href="/about">
         About
       </Link>
